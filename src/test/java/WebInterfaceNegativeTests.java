9f351297907b18474e86a10fb3df0090512cf296
Dmitriy <dimon-9_93@mail.ru>
2023-03-08 21:47:23 +0500

    Add WebDriverManager

--- src/test/java/WebInterfaceNegativeTests.java
+++ src/test/java/WebInterfaceNegativeTests.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,7 +16,8 @@ public class WebInterfaceNegativeTests {
     @BeforeAll
     static void setUpAll() {
 
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+
     }
 
     @BeforeEach
